9fc119b1e065f6b4052b0e3a59ad43f25124d0dd
Joseph Abad <jabadjka3@gmail.com>
2015-09-29 11:50:18 -0400

    code styling

--- math/Primes.java
+++ math/Primes.java
@@ -1,12 +1,18 @@
 package math;
 
 public class Primes {
+	/**
+	 * Find the next positive prime after the given starting point. If startPoint is negative, return null.
+	 * @param startPoint the number after which to search
+	 * @return the next prime after <code>startPoint</code>
+	 */
 	public static int findNextPrime(int startPoint) {
-		if(startPoint==1) return 2;
+		if(startPoint<0) return (Integer) null;
+		if(startPoint<=1) return 2;
 		startPoint++;
-		if(startPoint%2 == 0) startPoint ++;
+		if(startPoint%2==0) startPoint++;
 		while(!isPrime(startPoint)) {
-			startPoint += 2;
+			startPoint+=2;
 		}
 		return startPoint;
 	}
